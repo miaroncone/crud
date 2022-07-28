d08c29915fda5a4f8964d7cfb1a4d98e7dd815c0
Mia <miaroncone396@gmail.com>
2022-07-28 18:35:54 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+$connectionString = "mysql:host=localhost;port=3306;dbname=crud_productos";
+$user = "root";
+$pass = "";
+$pdo = new PDO ($connectionString, $user, $pass);
+$pdo -> setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//----------------------------------------------------------------
+$consulta = $pdo->prepare("SELECT * FROM productos order by fecha_creacion");
+$consulta->execute();
+$productos = $consulta-> fetchAll(PDO:: FETCH_ASSOC );
+//----------------------------------------------------------------
+echo "<pre>";
+var_dump($productos);
+echo "</pre>";
+//----------------------------------------------------------------
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mis Productos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+  <body>
+    <h1>CRUD de productos</h1>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
